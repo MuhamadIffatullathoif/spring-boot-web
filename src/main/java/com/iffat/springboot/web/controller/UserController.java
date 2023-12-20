09760aea89e44de10a4d3410afb60de25cf4e7dc
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-20 17:56:21 +0700

    02. Pass data to view

--- src/main/java/com/iffat/springboot/web/controller/UserController.java
+++ src/main/java/com/iffat/springboot/web/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.iffat.springboot.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
     @GetMapping("/details")
-    public String details() {
+    public String details(Model model) {
+        model.addAttribute("title","Hello world spring boot");
+        model.addAttribute("name","Muhamad");
+        model.addAttribute("lastname", "Iffat");
         return "details";
     }
 }
