1125cd55587355672129752bc14552ddaa677adc
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-21 06:59:42 +0700

    11. ModelAttribute Annotation

--- src/main/java/com/iffat/springboot/web/controller/UserController.java
+++ src/main/java/com/iffat/springboot/web/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,12 +25,20 @@ public class UserController {
 
     @GetMapping("/list")
     public String list(ModelMap modelMap){
-        List<User> users = Arrays.asList(
+        // List<User> users = Arrays.asList(
+        //        new User("Muhammad", "Iffat","Iffat@gmail.com"),
+        //        new User("Iffat", "Muhammad")
+        // );
+        // modelMap.addAttribute("users", users);
+        modelMap.addAttribute("title", "List of User");
+        return "list";
+    }
+
+    @ModelAttribute("users")
+    public List<User> usersModel() {
+        return Arrays.asList(
                 new User("Muhammad", "Iffat","Iffat@gmail.com"),
                 new User("Iffat", "Muhammad")
         );
-        modelMap.addAttribute("users", users);
-        modelMap.addAttribute("title", "List of User");
-        return "list";
     }
 }
