23ac1c146317c8a68e6f9836474a2fcf408a7e58
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-20 21:29:02 +0700

    08. Thymeleaf if directive

--- src/main/java/com/iffat/springboot/web/model/User.java
+++ src/main/java/com/iffat/springboot/web/model/User.java
@@ -3,6 +3,7 @@ package com.iffat.springboot.web.model;
 public class User {
     private String name;
     private String lastname;
+    private String email;
 
     public User(String name, String lastname) {
         this.name = name;
@@ -23,5 +24,13 @@ public class User {
 
     public void setLastname(String lastname) {
         this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
