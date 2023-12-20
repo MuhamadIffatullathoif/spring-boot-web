7a0cf615a8cad01a1e476cf673382fecf539ea87
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-20 18:10:42 +0700

    03. Rest Controller

---
+++ src/main/java/com/iffat/springboot/web/controller/UserRestController.java
@@ -0,0 +1,19 @@
+package com.iffat.springboot.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class UserRestController {
+    @GetMapping("/details2")
+    public Map<String, Object> details() {
+        Map<String, Object> body = new HashMap<>();
+        body.put("title","Hello World Spring boot");
+        body.put("name","Muhammad");
+        body.put("lastname","Iffat");
+        return body;
+    }
+}
