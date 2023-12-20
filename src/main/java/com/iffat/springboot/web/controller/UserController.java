2c8475bab189d4e959cbc7ec89e6e61f7831debc
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-20 18:37:19 +0700

    04. Request Mapping Annotation

--- src/main/java/com/iffat/springboot/web/controller/UserController.java
+++ src/main/java/com/iffat/springboot/web/controller/UserController.java
@@ -3,8 +3,10 @@ package com.iffat.springboot.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class UserController {
     @GetMapping("/details")
     public String details(Model model) {
--- src/main/java/com/iffat/springboot/web/controller/UserRestController.java
+++ src/main/java/com/iffat/springboot/web/controller/UserRestController.java
@@ -1,12 +1,14 @@
 package com.iffat.springboot.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
     @GetMapping("/details2")
     public Map<String, Object> details() {
