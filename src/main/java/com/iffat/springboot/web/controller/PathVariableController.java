4ced6dc807a7015ed70c657b11f5a1c472681954
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-21 09:12:16 +0700

    18. Inject values using @Value annotation

--- src/main/java/com/iffat/springboot/web/controller/PathVariableController.java
+++ src/main/java/com/iffat/springboot/web/controller/PathVariableController.java
@@ -2,6 +2,7 @@ package com.iffat.springboot.web.controller;
 
 import com.iffat.springboot.web.model.User;
 import com.iffat.springboot.web.model.dto.ParamDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -11,12 +12,32 @@ import java.util.Map;
 @RequestMapping("/api/var")
 public class PathVariableController {
 
+    @Value("${config.username}")
+    private String username;
+
+    @Value("${config.code}")
+    private Integer code;
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+
+    @GetMapping("/values")
+    public Map<String, Object> values(@Value("${config.message}") String message) {
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("code", code);
+        json.put("listOfValues", listOfValues);
+        json.put("message", message);
+        return json;
+    }
+
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
         ParamDto param = new ParamDto();
         param.setMessage(message);
         return param;
     }
+
 
     @GetMapping("/mix/{product}/{id}")
     public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id) {
