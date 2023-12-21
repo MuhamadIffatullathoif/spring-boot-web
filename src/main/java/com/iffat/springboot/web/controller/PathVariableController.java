a20bf10fbcdc4ce2e3734461dbbb8e749cf3ad69
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-21 09:56:35 +0700

    21. Spring Environment Object to read configuration

--- src/main/java/com/iffat/springboot/web/controller/PathVariableController.java
+++ src/main/java/com/iffat/springboot/web/controller/PathVariableController.java
@@ -2,7 +2,9 @@ package com.iffat.springboot.web.controller;
 
 import com.iffat.springboot.web.model.User;
 import com.iffat.springboot.web.model.dto.ParamDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -36,13 +38,19 @@ public class PathVariableController {
     @Value("#{${config.valuesMap}.price}")
     private Long price;
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/values")
     public Map<String, Object> values(@Value("${config.message}") String message) {
         Map<String, Object> json = new HashMap<>();
+        Long code2 = environment.getProperty("config.code", Long.class);
         json.put("username", username);
         json.put("code", code);
+        json.put("code2", code2);
         json.put("listOfValues", listOfValues);
         json.put("message", message);
+        json.put("message2", environment.getProperty("config.message"));
         json.put("valueList", valueList);
         json.put("valueString", valueString);
         json.put("valuesMap", valuesMap);
