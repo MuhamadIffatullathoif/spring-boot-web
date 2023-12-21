ee93e965f688e66c784338e1a40897b9eec38ac1
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-21 08:38:11 +0700

    15. PathVariable Annotation

---
+++ src/main/java/com/iffat/springboot/web/controller/PathVariableController.java
@@ -0,0 +1,19 @@
+package com.iffat.springboot.web.controller;
+
+import com.iffat.springboot.web.model.dto.ParamDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/var")
+public class PathVariableController {
+
+    @GetMapping("/baz/{message}")
+    public ParamDto baz(@PathVariable String message) {
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
+}
