580e886af5f368441455fce54aaf0c4d6649a6d1
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-21 08:43:09 +0700

    16. Get Multiple path parameters with PathVariable Annotation

--- src/main/java/com/iffat/springboot/web/controller/PathVariableController.java
+++ src/main/java/com/iffat/springboot/web/controller/PathVariableController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
@@ -15,5 +18,13 @@ public class PathVariableController {
         ParamDto param = new ParamDto();
         param.setMessage(message);
         return param;
+    }
+
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String product,@PathVariable Long id){
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id);
+        return json;
     }
 }
