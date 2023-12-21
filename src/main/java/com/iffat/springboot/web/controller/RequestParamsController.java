c1cb88a29cbf38cdf90bb62191945a3bd5d1722e
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-21 07:37:32 +0700

    13. Get Multiple parameters from URL with RequestParam

--- src/main/java/com/iffat/springboot/web/controller/RequestParamsController.java
+++ src/main/java/com/iffat/springboot/web/controller/RequestParamsController.java
@@ -1,6 +1,7 @@
 package com.iffat.springboot.web.controller;
 
 import com.iffat.springboot.web.model.dto.ParamDto;
+import com.iffat.springboot.web.model.dto.ParamMixDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,5 +16,13 @@ public class RequestParamsController {
         ParamDto params = new ParamDto();
         params.setMessage(message);
         return params;
+    }
+
+    @GetMapping("/bar")
+    public ParamMixDto bar(@RequestParam String text,@RequestParam Integer code){
+        ParamMixDto paramMixDto = new ParamMixDto();
+        paramMixDto.setMessage(text);
+        paramMixDto.setCode(code);
+        return paramMixDto;
     }
 }
